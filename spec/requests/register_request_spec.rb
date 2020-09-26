a82860dc6fd1456593ae87c5c400ac29dd367027
Luan F. R. Vicente <luanrvmood@gmail.com>
2020-09-26 05:51:47 -0300

    Implements the /register endpoint
    It only responds to 200 or 409 status for now

---
+++ spec/requests/register_request_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe "Registers", type: :request do
+  describe "POST /register" do
+    before :all do
+      Rails.application.load_seed
+    end
+
+    it "Returns the status code 201 on body" do
+      post "/api/v1/register", params: {email: "tsks@api.com",
+                                        password: "s",
+                                        password_confirmation: "s"}
+
+      parsed_body = JSON.parse response.body
+      expect(parsed_body).to include "status_code"
+      expect(parsed_body["status_code"]).to eq 201
+    end
+
+    it "Returns an authentication token" do
+      post "/api/v1/register", params: {email: "tsks@api.com",
+                                        password: "s",
+                                        password_confirmation: "s"}
+
+      parsed_body = JSON.parse response.body
+      expect(parsed_body).to include "token"
+    end
+
+    it "Returns the status code 409 on body" do
+      post "/api/v1/register", params: {email: "registered@api.com",
+                                        password: "s",
+                                        passord_confirmation: "s"}
+
+      parsed_body = JSON.parse response.body
+      expect(parsed_body["status_code"]).to eq 409
+    end
+  end
+end
