3fd4c6764bb5aac0a989e9350ac23f042971a959
Luan Ramos Vicente <luan.ramos@ferreri.co>
2022-12-22 10:55:02 -0300

    refactor: remaps auth routes to /signup and /signin

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :v1 do
-    post "/register", to: "register#create"
-    post "/login", to: "login#create"
+    post "/signup", to: "signup#create"
+    post "/signin", to: "signin#create"
     resources :tsks
   end
 end
