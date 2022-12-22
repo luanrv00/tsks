90b9260ea9d121fbbca9b1723a2801cc86fdde0a
Luan Ramos Vicente <luan.ramos@ferreri.co>
2022-12-22 10:59:41 -0300

    TMP: does not require password when saving users.tsks

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password validations: false
   validates :email, presence: true, uniqueness: true, allow_blank: false
-  validates :password, presence: true, allow_blank: false
+  # TODO: fix error requiring users password when saving user.tsks on POST /tsks
+  #validates :password, presence: true, allow_blank: false
   has_many :tsks
 end
