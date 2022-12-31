b5a87b42313149fe190a985e5d998aaaa6ba26f1
Luan Ramos Vicente <luanrvmood@gmail.com>
2022-12-31 20:43:20 -0300

    refactor: homepage tests

--- cypress/e2e/home.spec.js
+++ cypress/e2e/home.spec.js
@@ -1,12 +1,25 @@
-describe('Home', () => {
-  it('redirects to signup if not logged in', () => {
-    cy.visit('/')
-    cy.location('pathname').should('eq', '/signup')
+describe('Homepage', () => {
+  describe('when has session', () => {
+    cy.fixture('user').as('user')
+
+    beforeAll(() => {
+      localStorage.setItem('@tsks-user', user)
+    })
+
+    afterAll(() => {
+      localStorage.removeItem('@tsks-user')
+    })
+
+    it('redirects to tsks', () => {
+      cy.visit('/')
+      cy.location('pathname').should('eq', '/tsks')
+    })
   })
 
-  it('redirects to tsks list if logged in', () => {
-    localStorage.setItem('@tsks-user', JSON.stringify({id: true}))
-    cy.visit('/')
-    cy.location('pathname').should('eq', '/tsks')
+  describe('when has not session', () => {
+    it('redirects to signup', () => {
+      cy.visit('/')
+      cy.location('pathname').should('eq', '/signup')
+    })
   })
 })
