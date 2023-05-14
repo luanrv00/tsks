e6501f20ed47ea034f3ba76bb9319f2495aef1a1
Luan Ramos Vicente <luan.ramos@ferreri.co>
2023-05-14 07:54:31 -0300

    refactor: updates api url

--- lib/tsks/request.rb
+++ lib/tsks/request.rb
@@ -2,7 +2,7 @@ require "httparty"
 
 module Tsks
   class Request
-    @base_uri = "https://tsks-api.herokuapp.com/v1"
+    @base_uri = "https://tsks-api.onrender.com/v1"
 
     def self.base_uri
       @base_uri
