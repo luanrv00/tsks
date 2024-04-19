050c6477304bffe2d13fdf887470e72de8e9cd50
luan ramos vicente <luanramosvicente0@gmail.com>
2024-04-19 17:33:14 -0300

    setup: updates dev and test setups

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const {defineConfig} = require('cypress')
 
 module.exports = defineConfig({
   "e2e": {
-    "baseUrl": "http://web:3000",
+    "baseUrl": "http://app:3000",
     "spectPattern": "cypress/e2e/**/*.spec.js",
     "supportFile": false
   },
