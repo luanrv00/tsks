dd2a2722bc2ffcd98bf15fd692642888265345b9
luan ramos vicente <luanrv00@gmail.com>
2024-05-13 16:53:52 -0300

    test: rollback cypress config baseUrl

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,8 @@ const {defineConfig} = require('cypress')
 
 module.exports = defineConfig({
   "e2e": {
-    // "baseUrl": "http://app:3000",
-    "baseUrl": "http://localhost:3000",
+    // "baseUrl": "http://localhost:3000",
+    "baseUrl": "http://app:3000",
     "specPattern": "cypress/e2e/**/*.cy.js",
     "supportFile": false
   },
