236c4e47b85681b6fbd16e346c380e783dc3d0bc
luan ramos vicente <luanrv00@gmail.com>
2024-05-14 17:54:16 -0300

    fix: util folder name

--- utils/delete-current-user-at-browser/index.js
+++ utils/delete-current-user-at-browser/index.js
@@ -1,7 +1,5 @@
 const {NEXT_PUBLIC_TSKS_LOCAL_STORAGE_KEY} = process.env
 
 export function deleteCurrentUserAtBrowser() {
-  window.localStorage.removeItem(
-    NEXT_PUBLIC_TSKS_LOCAL_STORAGE_KEY
-  )
+  window.localStorage.removeItem(NEXT_PUBLIC_TSKS_LOCAL_STORAGE_KEY)
 }
