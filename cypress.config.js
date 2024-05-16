f7157a1b11933761c38650a478e85133ad71d0fe
luan ramos vicente <luanrv00@gmail.com>
2024-05-16 16:35:09 -0300

    test: avoids port usage conflict by changing e2e tests to run on 3002

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const {defineConfig} = require('cypress')
 module.exports = defineConfig({
   "e2e": {
     // "baseUrl": "http://localhost:3000",
-    "baseUrl": "http://app:3000",
+    "baseUrl": "http://app:3002",
     "specPattern": "cypress/e2e/**/*.cy.js",
     "supportFile": false
   },
