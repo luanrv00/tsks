2c731190fd378361ea232260f79cc47dd9a85af4
luan ramos vicente <luanrv00@gmail.com>
2024-05-16 17:33:00 -0300

    chore: updates delete button styles

--- components/tsk/index.js
+++ components/tsk/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './index.module.css'
-import {TskStatus} from '../'
+import {TskStatus, Button} from '../'
 
 export default function Tsk({id, tsk, context, status, handleDoing, handleDone, handleDelete}) {
   function onClick(e) {
@@ -21,14 +21,14 @@ export default function Tsk({id, tsk, context, status, handleDoing, handleDone, 
     <li className={styles.wrapper} onClick={onClick}>
       <div>
         <span className={styles.context}>@{context}</span>
-        <div className={styles.status}>
+        <div className={styles.tskWrapper}>
           <TskStatus status={status} />
           <p className={styles.tsk} data-testid='tsk'>
             {tsk}
           </p>
-          <button onClick={onClick}>delete</button>
         </div>
       </div>
+      <Button onClick={onClick} value='delete'/>
     </li>
   )
 }
