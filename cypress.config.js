97e8884eefa8e5e3da918338cad26ea30aecc1c5
luan ramos vicente <luanrv00@gmail.com>
2024-05-16 17:45:59 -0300

    setup: upadtes bin scripts

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const {defineConfig} = require('cypress')
 module.exports = defineConfig({
   "e2e": {
     // "baseUrl": "http://localhost:3000",
-    "baseUrl": "http://app:3002",
+    "baseUrl": "http://app_test_e2e:3002",
     "specPattern": "cypress/e2e/**/*.cy.js",
     "supportFile": false
   },
