a99c92936ad8443b1de3bc0e2c87d4c52406d769
luan ramos vicente <luanrv00@gmail.com>
2024-05-25 23:56:21 -0300

    setup: imports dev envs

--- lib/jwt_util.rb
+++ lib/jwt_util.rb
@@ -4,20 +4,20 @@ module JWTUtil
 	def self.create_auth_token user_payload
 		exp = Time.now.to_i - 4 * 3600
 		payload = user_payload.merge({exp: exp})
-		JWT.encode payload, nil, "none"
+		JWT.encode payload, ENV["AUTH_TOKEN_KEY"], "none"
 	end
 
 	def self.decode_auth_token token
-		JWT.decode token, nil, false
+		JWT.decode token, ENV["AUTH_TOKEN_KEY"], false
 	end
 
 	def self.create_refresh_token user_payload
-		exp = Time.now.to_i - 4 * 3600
+		exp = Time.now.to_i - 4 * 9000
 		payload = user_payload.merge({exp: exp})
-		JWT.encode payload, nil, "none"
+		JWT.encode payload, ENV["REFRESH_TOKEN_KEY"], "none"
 	end
 
 	def self.decode_refresh_token token
-		JWT.decode token, nil, false
+		JWT.decode token, ENV["REFRESH_TOKEN_KEY"], false
 	end
 end
