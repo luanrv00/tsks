ebeaa0402e5e44c86a431165bd08b9a283d2a8c4
luan ramos vicente <luanrv00@gmail.com>
2024-05-26 18:18:50 -0300

    feat: adds route POST for refresh token endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :v1 do
     post "/signup", to: "signup#create"
     post "/signin", to: "signin#create"
+    post "/refresh_token", to: "refresh_token#create"
     resources :tsks
   end
 end
