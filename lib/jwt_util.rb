3a3b15db822649f36589c662d7a471f3ae034a10
luan ramos vicente <luanrv00@gmail.com>
2024-05-27 00:59:25 -0300

    refactor: creates auth and refresh tokens with encryption

--- lib/jwt_util.rb
+++ lib/jwt_util.rb
@@ -4,7 +4,7 @@ module JWTUtil
 	def self.create_auth_token user_payload
 		exp = 1.minute.from_now.to_i
 		payload = user_payload.merge({exp: exp})
-		JWT.encode payload, ENV["AUTH_TOKEN_KEY"], "none"
+		JWT.encode payload, ENV["AUTH_TOKEN_KEY"], "HS256"
 	end
 
 	def self.decode_auth_token token
@@ -14,7 +14,7 @@ module JWTUtil
 	def self.create_refresh_token user_payload
 		exp = 1.week.from_now.to_i
 		payload = user_payload.merge({exp: exp})
-		JWT.encode payload, ENV["REFRESH_TOKEN_KEY"], "none"
+		JWT.encode payload, ENV["REFRESH_TOKEN_KEY"], "HS256"
 	end
 
 	def self.decode_refresh_token token
