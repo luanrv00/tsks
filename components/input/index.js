bcc784bb2609ead08cfed1ba9a2ec16b430a92d6
luan ramos vicente <luanrv00@gmail.com>
2024-06-06 07:29:06 -0300

    refactor: extracts input behavior to InputBase then extends it

--- components/input/index.js
+++ components/input/index.js
@@ -1,18 +1,65 @@
 import React from 'react'
 import styles from './index.module.css'
 
-export function Input({value, placeholder, type, onChange, hasEmptyError, hasInvalidEmailError}) {
+function InputBase({value, placeholder, type, onChange, hasEmptyError, hasInvalidEmailError, style}) {
+  const specificStyle = style === 'small' ? styles.smallInput :
+                        style === 'medium' ? styles.mediumInput :
+                        null
+
+  const inputStyles = `${styles.input} ${specificStyle}`
+
   return (
     <>
-    <input
-      className={styles.input}
+      <input
+        className={inputStyles}
+        value={value}
+        type={type}
+        placeholder={placeholder}
+        onChange={onChange}
+      />
+      {hasEmptyError && (<span>required</span>)}
+      {hasInvalidEmailError && (<span>invalid email</span>)}
+    </>
+  )
+}
+
+export function Input({value, placeholder, type, onChange, hasEmptyError, hasInvalidEmailError}) {
+  return (
+    <InputBase
       value={value}
       type={type}
       placeholder={placeholder}
       onChange={onChange}
+      hasEmptyError={hasEmptyError}
+      hasInvalidEmailError={hasInvalidEmailError}
     />
-    {hasEmptyError && (<span>required</span>)}
-    {hasInvalidEmailError && (<span>invalid email</span>)}
-    </>
+  )
+}
+
+export function MediumInput({value, placeholder, type, onChange, hasEmptyError, hasInvalidEmailError}) {
+  return (
+    <InputBase
+      value={value}
+      type={type}
+      placeholder={placeholder}
+      onChange={onChange}
+      hasEmptyError={hasEmptyError}
+      hasInvalidEmailError={hasInvalidEmailError}
+      style='medium'
+    />
+  )
+}
+
+export function SmallInput({value, placeholder, type, onChange, hasEmptyError, hasInvalidEmailError}) {
+  return (
+    <InputBase
+      value={value}
+      type={type}
+      placeholder={placeholder}
+      onChange={onChange}
+      hasEmptyError={hasEmptyError}
+      hasInvalidEmailError={hasInvalidEmailError}
+      style='small'
+    />
   )
 }
