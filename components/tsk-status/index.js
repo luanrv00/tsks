3f66e64d3fe4f860604b7ece0a86e8fe20581e7d
luan ramos vicente <luanrv00@gmail.com>
2024-06-06 07:57:34 -0300

    feat: fix tsk status height aligment

--- components/tsk-status/index.js
+++ components/tsk-status/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import styles from './index.module.css'
 
 export function TskStatus({status}) {
-  return <>{availableStatus[status]}</>
+  const style = status === 'done' ? styles.doneStatus : null
+  return <span className={style}>{availableStatus[status]}</span>
 }
 
 const availableStatus = {
