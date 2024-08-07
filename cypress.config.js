5a01625d66a5b0d578c0bc86f49433299b415669
luan ramos vicente <luanrv00@gmail.com>
2024-08-07 13:06:48 -0300

    wip: tries to run on localhost ip on ci

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const {defineConfig} = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://127.0.0.1:3000',
     //baseUrl: 'http://app:3000',
     specPattern: 'cypress/e2e/**/*.cy.js',
     supportFile: 'cypress/support/index.js',
