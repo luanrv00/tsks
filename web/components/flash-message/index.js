f5eb819a1d340beb184d02ce945cdb6ef78e5ec8
luan ramos vicente <luanrv00@gmail.com>
2024-08-23 07:32:53 -0300

    feat: adds styles for FlashMessage

--- web/components/flash-message/index.js
+++ web/components/flash-message/index.js
@@ -1,3 +1,6 @@
+import styles from './index.module.css'
+
 export function FlashMessage({type, message}) {
-  return <>{message ? <p className={type}>{message}</p> : null}</>
+  const specificStyle = type === 'success' ? styles.success : styles.error
+  return <>{message ? <p className={`${styles.flash} ${specificStyle}`}>{message}</p> : null}</>
 }
