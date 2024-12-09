2030fe26b8e21f6007d85198919100addb193977
luan ramos vicente <luanfrv9@gmail.com>
2024-12-09 18:58:25 -0300

    fix: encode jose alg key

--- web/utils/create-access-token/index.js
+++ web/utils/create-access-token/index.js
@@ -3,10 +3,12 @@ import * as jose from 'jose'
 const ACCESS_TOKEN_KEY = process.env.ACCESS_TOKEN_KEY
 
 export async function createAccessToken(email) {
+  const accessKey = new TextEncoder().encode(ACCESS_TOKEN_KEY)
+
   const token = await new jose.SignJWT({email})
     .setProtectedHeader({alg: 'HS256'})
     .setExpirationTime('1h')
-    .sign(ACCESS_TOKEN_KEY)
+    .sign(accessKey)
 
   return token
 }
--- web/utils/create-refresh-token/index.js
+++ web/utils/create-refresh-token/index.js
@@ -3,10 +3,12 @@ import * as jose from 'jose'
 const REFRESH_TOKEN_KEY = process.env.REFRESH_TOKEN_KEY
 
 export async function createRefreshToken(email) {
+  const refreshKey = new TextEncoder().encode(REFRESH_TOKEN_KEY)
+
   const token = await new jose.SignJWT({email})
     .setProtectedHeader({alg: 'HS256'})
     .setExpirationTime('10h')
-    .sign(REFRESH_TOKEN_KEY)
+    .sign(refreshKey)
 
   return token
 }
