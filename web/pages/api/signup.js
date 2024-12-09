3b700fbb35f0afaf2cce4a44894cefdad01b8e21
luan ramos vicente <luanfrv9@gmail.com>
2024-12-09 18:36:09 -0300

    feat: save user from signup

--- web/pages/api/signup.js
+++ web/pages/api/signup.js
@@ -1,7 +1,14 @@
-export default function handler(req, res) {
-  try {
-    res.status(201).json({ok: true})
-  } catch(e) {
-    res.status(500).json({message: '500 Internal Server Error'})
+import { apiUserSignup } from "../../services"
+
+export default async function handler(req, res) {
+  const {email, password} = req.body
+  const user = await apiUserSignup({email, password})
+
+  if (user) {
+    try {
+      res.status(201).json({ ok: true, data: {user} })
+    } catch (e) {
+      res.status(500).json({ ok: false, message: '500 Internal Server Error' })
+    }
   }
 }
