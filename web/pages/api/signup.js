0dfb3538b98f3636b657c40888383b3a142dbbbe
luan ramos vicente <luanfrv9@gmail.com>
2024-12-09 01:55:22 -0300

    fix: api/signup return

--- web/pages/api/signup.js
+++ web/pages/api/signup.js
@@ -1,3 +1,7 @@
 export default function handler(req, res) {
-  return res.status(202)
+  try {
+    res.status(201).json({ok: true})
+  } catch(e) {
+    res.status(500).json({message: '500 Internal Server Error'})
+  }
 }
